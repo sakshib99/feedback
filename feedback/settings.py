549b7b1aa1feac7af5ef4d9b81155c47fa396348
sakshi bhise <49953489+sakshib99@users.noreply.github.com>
2025-05-21 20:57:42 +0530

    Update settings.py

--- feedback/settings.py
+++ feedback/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'feedback' ,
         'USER':'root',
-        'PASSWORD':'Sakshi123',
+        'PASSWORD':'',
         'HOST':'localhost',
         'PORT':'3306',
         'default-character-set':'utf8',
